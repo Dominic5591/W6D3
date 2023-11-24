56c06f0621f649f6d9b9be2d4cc1a74cafd039cd
Dominic <domcashiola@gmail.com>
2023-11-24 14:17:51 -0500

    Finished phase 2 Users and Artworks API

--- art-share-api-main/Art_share_project/config/routes.rb
+++ art-share-api-main/Art_share_project/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :users
+  resources :users, only: [:create, :destroy, :index, :show, :update]
+
+  resources :artworks, only: [:create, :destroy, :index, :show, :update]
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- art-share-api-main/Art_share_project/db/seeds.rb
+++ art-share-api-main/Art_share_project/db/seeds.rb
@@ -7,3 +7,52 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+# ApplicationRecord.transaction do
+
+  # These `destroy_all` commands are not necessary if you use `rails
+  # db:seed:replant`
+  puts 'Destroying tables...'
+  # ArtworkCollection.destroy_all
+  # Collection.destroy_all
+  # Like.destroy_all
+  # Comment.destroy_all
+  ArtworkShare.destroy_all
+  Artwork.destroy_all
+  User.destroy_all
+
+  # Reset the id (i.e., primary key) counters for each table to start at 1
+  # (helpful for debugging)
+  puts 'Resetting id sequences...'
+  %w(users artworks artwork_shares comments likes collections artwork_collections).each do |table_name|
+    ApplicationRecord.connection.reset_pk_sequence!(table_name)
+  end
+
+
+
+
+
+  puts 'Seeding the DB...'
+  user1 = User.create!(username: 'robert')
+  user2 = User.create!(username: 'sally')
+  user3 = User.create!(username: 'bill')
+  artwork1 = Artwork.create!(title: 'nighthawks', image_url: 'google.com', artist_id: user1.id)
+  artwork2 = Artwork.create!(title: 'mona lisa', image_url: 'google1.com', artist: user2)
+  ArtworkShare.create!(artwork: artwork1, viewer: user2)
+  ArtworkShare.create!(artwork_id: artwork2.id, viewer_id: user3.id)
+  # comment1 = Comment.create!(body: 'great!', author: user1, artwork: artwork1)
+  # comment2 = Comment.create!(body: 'another great one', author: user2, artwork: artwork2)
+  # Like.create!(liker: user1, likeable_id: comment1.id, likeable_type: 'Comment')
+  # Like.create!(liker: user2, likeable_id: artwork2.id, likeable_type: 'Artwork')
+  # Like.create!(liker: user1, likeable_id: artwork2.id, likeable_type: 'Artwork')
+  # Like.create!(liker: user2, likeable_id: comment1.id, likeable_type: 'Comment')
+  # collection1 = Collection.create!(name: "My first Collection!", user: user1)
+  # collection2 = Collection.create!(name: "Birds, Birds, Birds", user: user2)
+  # ArtworkCollection.create!(collection: collection1, artwork: artwork1)
+  # ArtworkCollection.create!(collection: collection1, artwork: artwork2)
+  # ArtworkCollection.create!(collection: collection2, artwork: artwork2)
+
+  puts 'Done!'
+
+# end
